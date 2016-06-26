8e51b45d9d9240563cf34931d12a317ed2bbb437
lawrence <tc.first@163.com>
2016-06-26 18:01:52 +0800

    push all code into github

---
+++ MVCWebAPI/Filters/MyFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace Kuiyu.MvcWebAPI.Filters
+{
+    public class MyFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            #region 2.得到传给WEBAPI的参数值
+            HttpContextWrapper content = (HttpContextWrapper)actionContext.Request.Properties["MS_HttpContext"];
+            //假设POST过来两个参数值 Plain 和Signature
+            string Plain = content.Request.Form["Plain"]; 
+            string Signature = content.Request.Form["Signature"];
+            #endregion
+
+            base.OnActionExecuting(actionContext);
+        }
+    }
+}
